d03f0766f50778fcff3b7636aaa28ea31cb71a81
haappi <74637670+haappi@users.noreply.github.com>
2022-03-20 15:00:23 -0500

    ~~blacken~~ **whiten**

--- modules/antiabuse.py
+++ modules/antiabuse.py
@@ -12,12 +12,17 @@ class Abuse(commands.Cog):
     async def on_member_update(self, before: discord.Member, after: discord.Member):
         if before.is_timed_out():
             return
-        async for entry in before.guild.audit_logs(limit=2, action=discord.AuditLogAction.member_update):
+        async for entry in before.guild.audit_logs(
+            limit=2, action=discord.AuditLogAction.member_update
+        ):
             if entry.target == after:
                 if isinstance(entry.reason, str):
                     if len(entry.reason) > 4:
                         return
-                await after.edit(timed_out_until=None, reason="Supply a reason longer than 5 characters.")
+                await after.edit(
+                    timed_out_until=None,
+                    reason="Supply a reason longer than 5 characters.",
+                )
 
 
 async def setup(bot):
