35844c7deb3f667dc8e0321c09a3f54f46e092be
haappi <74637670+haappi@users.noreply.github.com>
2022-12-17 02:45:14 -0600

    done

--- modules/smp_listener.py
+++ modules/smp_listener.py
@@ -9,6 +9,7 @@ import discord
 class SMPListener(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        self.channel: discord.TextChannel = None
         self.redis: aioredis.ConnectionPool = bot.redis
         self.ready = False
 
@@ -34,6 +35,7 @@ class SMPListener(commands.Cog):
     async def on_ready(self):
         if not self.ready:
             self.ready = True
+            self.channel = self.bot.get_channel(927300714508730418)
             pubsub = self.redis.pubsub()
             await pubsub.subscribe("discord")
             future = asyncio.create_task(self.reader(pubsub))
@@ -45,10 +47,33 @@ class SMPListener(commands.Cog):
                 async with async_timeout.timeout(1):
                     message = await channel.get_message(ignore_subscribe_messages=True)
                     if message is not None:
-                        await self.bot.get_channel(927300714508730418).send(str(message["data"]), allowed_mentions=discord.AllowedMentions.none())
+                        await self.handle_type(bytes(message["data"]).decode("utf-8"))
                     await asyncio.sleep(0.01)
             except asyncio.TimeoutError:
                 pass
+
+
+    async def handle_type(self, message: str):
+        _type = message.split(";")[0]
+        message = message.split(";")[1]
+        match _type:
+            case "online":
+                await self.channel.edit(topic=f"""Join at **smp.quack.tk**! **{message}/25** Players Online.
+                        19132 - Bedrock (Default)
+                        25565 - Java (Default)""")
+            case "chat":
+                await self.channel.send(message, allowed_mentions=discord.AllowedMentions.none())
+            case "join":
+                await self.channel.send(embed=discord.Embed(description=f"{message} joined the game.", colour=discord.Colour.green()), allowed_mentions=discord.AllowedMentions.none())
+            case "leave":
+                await self.channel.send(embed=discord.Embed(description=f"{message} left the game.", colour=discord.Colour.orange()), allowed_mentions=discord.AllowedMentions.none())
+            case "death":
+                await self.channel.send(embed=discord.Embed(description=f"{message}", colour=discord.Colour.red()), allowed_mentions=discord.AllowedMentions.none())
+            case "advancement":
+                await self.channel.send(embed=discord.Embed(description=f"{message}", colour=discord.Colour.blue()), allowed_mentions=discord.AllowedMentions.none())
+
+
+
 
 
 
